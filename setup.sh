5952c92053ca58a1647eba8e06ae0314070df49d
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2022-02-01 14:04:42 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get install default-jre python3 golang-go nodejs gcc snapd
+sudo pacman -S default-jre python3 golang-go nodejs gcc snapd
+sudo dnf install default-jre python3 golang-go nodejs gcc snapd
+sudo snap install flutter --classic
+flutter
+clear
+echo "done!"
